92c27fcbb0dfb1e8ded860ae9fea97cfb1d5e5a2
jcorm714 <jcorm714@gmail.com>
2020-08-31 13:02:07 -0400

    Belady's algorithm

--- main.cpp
+++ main.cpp
@@ -1,16 +1,32 @@
 #include<iostream>
 #include<cstdlib>
+#include<vector>
+#include<memory>
 
 #define PAGE_STREAM_SIZE  100
+#define NOTHING_IN_FRAME  17
 
-int determineFurthestPage(unsigned int pageStream[], unsigned int activePages[], int pageStreamStart);
-void resetPageFrames(unsigned int** pageFrames, int width, int height);
-void displayPageFrame(unsigned int** pageFrames, int width, int height);
+class Row {
+    public:
+        std::unique_ptr<unsigned int[]> row;
+        Row(int size) {
+            this->row = std::unique_ptr<unsigned int[]>(new unsigned int[size]);
+            for (int i = 0; i < size; i++) {
+                this->row[i] = NOTHING_IN_FRAME;
+            }
+        } 
+};
+
+
+int determineFurthestPage(unsigned int pageStream[], std::shared_ptr<Row>&, int pageStreamStart, int pageStreamLength, int activePagesLength);
+void displayPageFrame(std::shared_ptr<Row>& currRow, int page, int rowLength);
+int findEmptySlot(std::shared_ptr<Row>& row, int length);
+bool pageInMemory(int page, std::shared_ptr<Row>&,  int length);
 
 int main(){
 
     bool isValid = false;
-    unsigned int numPages = 0;
+    int numPages = 0;
     unsigned int addrs[PAGE_STREAM_SIZE];
     unsigned int pageStream[PAGE_STREAM_SIZE];
    
@@ -26,10 +42,11 @@ int main(){
 
     //create allocate enough space in the array for 
     //for each page to be run in the array
-    unsigned int **pageFrames = new unsigned int*[numPages];
-    for(int i = 0; i < numPages; i++){
-        pageFrames[i] = new unsigned int[PAGE_STREAM_SIZE];
-    }
+    std::vector<std::shared_ptr<Row>> pageFrames;
+    
+    //initialize empty row
+    std::shared_ptr<Row> r = std::shared_ptr<Row>(new Row(numPages));
+    pageFrames.push_back(r);
 
     //initialize random stream
     for(int i =0; i < PAGE_STREAM_SIZE; i++){
@@ -40,65 +57,65 @@ int main(){
 
        pageStream[i] = pNum;
     }
-    
-    std::cout << "address\t\t" << "page\t" << "offset\n";
-    for(int i : addrs){
-       int offset = i % 4096;
-       int pNum = (i - offset)/4096;
-       std::cout <<i << "\t\t" << pNum << '\t' << offset << '\n';
-    }
+   
+    int currentRow = 0;
+    int pageFaults = 0;
+    for (int i = 0; i < PAGE_STREAM_SIZE; i++) {
+
+        //only switch pages if the page is not in memory
+        if (pageInMemory(pageStream[i], pageFrames.back(), numPages)) {
+            displayPageFrame(pageFrames.back(), pageStream[i], numPages);
+
+            continue;
+        }
 
 
-    //free up memory
-    for(int i = 0; i < numPages; i++){
-        delete[] pageFrames[i];
+        int idx = findEmptySlot(pageFrames.back(), numPages);
+        if (idx >= 0) {
+            pageFrames.back()->row[idx] = pageStream[i];
+
+        }
+        else {
+           
+            int frameToSwitch = determineFurthestPage(pageStream, pageFrames.back(), i, PAGE_STREAM_SIZE, numPages);
+            pageFrames.back()->row[frameToSwitch] = pageStream[i];
+
+
+        }
+      
+        std::shared_ptr<Row> newRow = std::shared_ptr<Row>(new Row(numPages));
+        for (int i = 0; i < numPages; i++) {
+            newRow->row[i] = pageFrames.back()->row[i];
+        }
+        pageFrames.push_back(newRow);
+     
+        displayPageFrame(pageFrames.back(), pageStream[i], numPages);
+      
     }
-    delete[] pageFrames;
+
+    std::cout << "Number of page faults for Belady: " << pageFaults;
+    pageFrames.clear();
 
 
     return 0;
 }
 
-void resetPageFrames(unsigned int** pageFrames, int width, int height){
-    for(int i = 0; i < width; i++){
-        for(int j = 0; j< height; j++){
-            pageFrames[i][j] = NULL;
-        }
-    }
-}
-
-void displayPageFrame(unsigned int** pageFrames, int width, int height){
-     for(int i = 0; i < width; i++){
-        for(int j = 0; j< height; j++){
-            if(pageFrames[i][j] == NULL){
-                std::cout<< ' ';
-            } else {
-                std::cout << pageFrames[i][j] << ' ';
-            }
-        }
-        std::cout<< '\n';
-    }
-}
-
-
-int determineFurthestPage(unsigned int pageStream[], unsigned int activePages[], int pageStreamStart){
-    
-    int pageStreamLength = sizeof(pageStream)/sizeof(pageStream[0]);
-    int activePagesLength = sizeof(activePages)/sizeof(activePages[0]);
+int determineFurthestPage(unsigned int pageStream[], std::shared_ptr<Row>& activePages, int pageStreamStart, int pageStreamLength, int activePagesLength)
+{
     int furthestPage = -1;
     int greatestDistance = -1;
     int greatestPageIdx = -1;
 
 
-    for(int i =0; i < activePagesLength; i++){
-        unsigned int currentPage = activePages[i];
+    for (int i = 0; i < activePagesLength; i++) {
+        unsigned int currentPage = activePages->row[i];
         //starting at the current point in the page stream
         //find the page with the greatest distance from the point
-        for(int j=pageStreamStart; j< pageStreamLength; j++){
+        for (int j = pageStreamStart; j < pageStreamLength; j++) {
             unsigned int currPage = pageStream[j];
-            if(currentPage == currPage){
+            if (currentPage == currPage) {
                 int dist = j - pageStreamStart;
-                if(dist > greatestDistance){
+                if (dist > greatestDistance) {
                     greatestDistance = dist;
                     greatestPageIdx = i;
                     break;
@@ -109,3 +126,45 @@ int determineFurthestPage(unsigned int pageStream[], unsigned int activePages[],
     }
     return greatestPageIdx;
 }
+
+void displayPageFrame(std::shared_ptr<Row>& currRow, int page, int rowLength){
+
+    std::cout << page << "  ";
+    
+    for (int i = 0; i < rowLength; i++) {
+        if (currRow->row[i] == NOTHING_IN_FRAME) {
+            std::cout << 'x' << "  ";
+        }
+        else {
+            std::cout << currRow->row[i] << "  ";
+        }
+    }
+    std::cout << '\n';
+
+}
+
+int findEmptySlot(std::shared_ptr<Row>& row, int length)
+{
+    int i = 0;
+    for (int i = 0; i < length; i++) {
+        if (row->row[i] == NOTHING_IN_FRAME) {
+            return i;
+        }
+    }
+    return -1;
+    
+
+}
+
+bool pageInMemory(int page , std::shared_ptr<Row>& row, int length)
+{
+
+   
+    for (int i = 0; i < length; i++) {
+        if (row->row[i] == page) {
+            return true;
+        }
+    }
+    return false;
+}
+
